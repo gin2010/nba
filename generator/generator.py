3e8bbfc4352b2ab0dd92d60d678431e7ba745f94
challenge24_hp <gin2010@yeah.net>
2019-10-15 16:59:19 +0800

    generator v1.1

--- generator/generator.py
+++ generator/generator.py
@@ -11,9 +11,10 @@ from tool.generatorCase import Generator
 
 
 
-class Generator_tjcx(Generator):
+class Generator_tjcx_kpcx(Generator):
 
     # 修改父类的模板
+    # 统计查询接口--开票查询
     def __init__(self):
         super().__init__()  # 初始化父类
 
@@ -24,7 +25,48 @@ class Generator_tjcx(Generator):
         return json.dumps(temp, ensure_ascii=False)
 
 
+class Generator_tjcx_kpts(Generator):
+
+    # 修改父类的模板
+    # 统计查询接口--发票推送
+    def __init__(self):
+        super().__init__()  # 初始化父类
+
+
+    # 修改父类的update_temp方法
+    def update_temp(self,temp,step,fphm,inner_key,inner_value):
+        try:
+            temp = int(inner_value)
+        except Exception:
+            temp = inner_value
+        return json.dumps(temp, ensure_ascii=False)
+
+
+class Generator_fs(Generator):
+
+    # 修改父类的模板
+    # 统计查询接口--开票查询
+    def __init__(self):
+        super().__init__()  # 初始化父类
+
+
+    # 修改父类的update_temp方法
+    def update_temp(self,temp,step,fphm,inner_key,inner_value):
+        temp[inner_key] = inner_value
+        return json.dumps(temp, ensure_ascii=False)
+
+
+
 if __name__ == "__main__":
-    print(sys.path)
-    generate = Generator_tjcx()
+    # 统计查询接口--开票查询
+    # generate = Generator_tjcx_kpcx()
+    # generate.generate_single_case()
+
+    # 统计查询接口--发票推送
+    # generate = Generator_tjcx_kpts()
+    # generate.generate_single_case()
+
+    # 非税接口
+    generate = Generator_fs()
     generate.generate_single_case()
+
