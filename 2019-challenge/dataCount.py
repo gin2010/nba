9921ecda271179ec9df5403bc5d7037f8c9bb6cb
challenge24_hp <gin2010@yeah.net>
2019-11-13 21:34:07 +0800

    20191113

--- 2019-challenge/dataCount.py
+++ 2019-challenge/dataCount.py
@@ -6,35 +6,51 @@
 
 import xlrd,xlwt,os,logging
 import pandas as pd
-import re
+import numpy as np
+import re,time
 from xlutils.copy import copy
 
 ##配置文件
-LOG_LEVEL = 0  # DEBUG(10)<INFO(20)<WARN(30)<ERROR(40)<CRITICAL(50)
-
+LOG_LEVEL = 30  # DEBUG(10)<INFO(20)<WARN(30)<ERROR(40)<CRITICAL(50)
+TITLES = ['序号', '测试产品线', '工作项目/任务', '任务来源', '任务性质', '测试负责人', '研发负责人', '本周工作目标',
+          '实际完成时间', '实际完成情况', '测试人员', '工时', '事业部', '亮点', '问题点', '修改内容简述', '备注']
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class DataCount(object):
 
     def __init__(self):
-        self.department = "系统测试部"
-        self.excel_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"测试评价部-系统测试部-测试周报.xlsx")
+        self.weekly_xt = "测试评价部-系统测试部-测试周报.xlsx"
+        self.weekly_ck = "测试评价部-测试开发部-测试周报.xls"
+        self.temp_xt = os.path.join(DIR_PATH,"temp","data_xt.xls")
+        self.temp_ck = os.path.join(DIR_PATH,"temp","data_ck.xls")
+        self.total = os.path.join(DIR_PATH,"temp","total.xls")
         logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s- %(levelname)s- %(message)s:')
         self.logger = logging.getLogger(__name__)
-        # 配置获取最大列数
+        # 配置获取周报最大列数(由于从0开始，所以实际是17列)
         self.max_column = 16
-        # fomatting_info 参数不支持xlsx
-        # self.wb = xlrd.open_workbook(self.excel_path, formatting_info=True)
-        self.wb = xlrd.open_workbook(self.excel_path)
-        self.sheets_list = self.wb.sheet_names()
-        self.new_wb = xlwt.Workbook(encoding='utf-8')
 
 
-    # 填充合并单元格、拆分多值单元格为多行
-    def _fill_merge_cell_and_split(self,sheet_name):
-        sheet = self.new_wb.add_sheet(sheet_name,cell_overwrite_ok=True)
-        ws = self.wb.sheet_by_name(sheet_name)
-        self.logger.debug(sheet_name)
+    def _open_excel(self,excel_name):
+        excel_path = os.path.join(DIR_PATH, excel_name)
+        if excel_name.endswith("xls"):
+            # fomatting_info 参数不支持xlsx，但xls格式如果不加此参数则无法获得合并区域
+            wb = xlrd.open_workbook(excel_path,formatting_info=True)
+        else:
+            wb = xlrd.open_workbook(excel_path)
+        sheets_list = wb.sheet_names()
+        department = excel_name[(excel_name.find("-")+1):(excel_name.find("-",excel_name.find("-")+1))]
+        self.logger.debug("department:{}".format(department))
+        return wb,department
+
+
+    # 填充合并单元格、拆分多值单元格为多行，一次处理的是一个sheet
+    def _fill_merge_cell_and_split(self,wb,sheet_name,wb_new,department):
+        # 新建写入excel的sheet
+        sheet = wb_new.add_sheet(sheet_name,cell_overwrite_ok=True)
+        # 打开周报对应sheet
+        ws = wb.sheet_by_name(sheet_name)
+        self.logger.debug("正在处理sheet:{}....".format(sheet_name))
         # 获得合并单元格区域的列表
         merge_cell = ws.merged_cells
         self.logger.debug(merge_cell)
@@ -56,13 +72,13 @@ class DataCount(object):
             if len(self._del_blank_and_return(ws.cell(r,10).value)) > 1:
                 name = self._del_blank_and_return(ws.cell(r,10).value)
                 self.logger.debug(name)
-                workhour = self._del_blank_and_return(ws.cell(r,11).value)
+                workhour = self._del_blank_and_return(ws.cell(r,11).value,False)
                 self.logger.debug(workhour)
                 for i in range(len(self._del_blank_and_return(ws.cell(r,10).value))):
                     for c in range(self.max_column + 1):
                         row_value, column_value = self._get_cell_value((r, c), merge_sections)
                         sheet.write(target_r, c, ws.cell(row_value, column_value).value)
-                    sheet.write(target_r, 10, name[i])
+                    sheet.write(target_r, 10, self._del_blank_and_return(name[i]))
                     try:
                         sheet.write(target_r, 11, workhour[i])
                     except IndexError:
@@ -70,19 +86,50 @@ class DataCount(object):
                         sheet.write(target_r, 11, 0)
                     target_r += 1
                     # 将sheet_name值添加为新列“期间”
-                    sheet.write(target_r, self.max_column + 1, sheet_name)
-                    sheet.write(target_r, self.max_column + 2, self.department)
+                    # sheet.write(target_r-1, self.max_column + 1, sheet_name)
+                    # sheet.write(target_r-1, self.max_column + 2, department)
             else:
                 for c in range(self.max_column + 1):
                     row_value, column_value = self._get_cell_value((r, c), merge_sections)
-                    sheet.write(target_r, c, ws.cell(row_value, column_value).value)
+                    if c==10:
+                        sheet.write(target_r, c, self._del_blank_and_return(ws.cell(row_value, column_value).value))
+                    else:
+                        sheet.write(target_r, c, ws.cell(row_value, column_value).value)
                 target_r += 1
             #将sheet_name值添加为新列“期间”
-            sheet.write(target_r,self.max_column+1,sheet_name)
-            sheet.write(target_r, self.max_column + 2, self.department)
+            # sheet.write(target_r-1,self.max_column+1,sheet_name)
+            # sheet.write(target_r-1, self.max_column + 2, department)
+        # 列标题统一
         sheet.write(0,self.max_column + 1,"期间")
         sheet.write(0, self.max_column + 2, "二级部门")
-        # 加一个对列标题的处理
+        for c in range(self.max_column + 1):
+            sheet.write(0, c, TITLES[c])
+        # 写入期间与二级部门
+        for i in range(1,target_r):
+            sheet.write(i,self.max_column+1,sheet_name)
+            sheet.write(i, self.max_column + 2, department)
+
+
+
+    def _clear_data(self):
+        # 汇总并清洗系统测试周报
+        wb_xt,department = self._open_excel(self.weekly_xt)
+        # 新建xlwt表格来写入excel数据
+        wb_new_xt = xlwt.Workbook(encoding='utf-8')
+        for sheet_name in wb_xt.sheet_names():
+            self._fill_merge_cell_and_split(wb_xt, sheet_name,wb_new_xt,department)
+            wb_new_xt.save(self.temp_xt)
+        self.logger.warning("系统测试周报数据清洗成功！！！")
+
+        # 汇总并清洗测试开发周报
+        wb_ck,department = self._open_excel(self.weekly_ck)
+        # 新建xlwt表格来写入excel数据
+        wb_new_ck = xlwt.Workbook(encoding='utf-8')
+        for sheet_name in wb_ck.sheet_names():
+            self._fill_merge_cell_and_split(wb_ck, sheet_name,wb_new_ck,department)
+            wb_new_ck.save(self.temp_ck)
+        self.logger.warning("测试开发周报数据清洗成功！！！")
+
 
 
     def _get_cell_value(self,section,merge_sections):
@@ -104,80 +151,97 @@ class DataCount(object):
             return section
 
 
-    def _del_blank_and_return(self,value):
+    def _del_blank_and_return(self,value,is_str=True):
         '''
         处理多值单元格：对多值单元格格式化，去掉两边的空格、回车符，并将中间的空格替换为"\n"
         :param self:
         :param value:某个单元格的值
         :return:格式化后的值
         '''
+        invalid_list= ["河北","总部","河北：",""]
         replace_blank = lambda x: re.sub(r"\s{2,}", "\n", x)
-        value = replace_blank(str(value))
+        del_bracket = lambda y: re.sub(r"[\\(\\（].*?[\\)\\）]", "", y)
+        del_order = lambda z: re.sub(r'^\d\.','',z)
+        value = replace_blank(str(value)) #将多个值之前的回车、空格转为一个回车
+        value = del_bracket(value)  # 去掉值后面的括号
+        if is_str:
+            value = del_order(value)  # 去掉值最前面的1.或2.
+        self.logger.debug(value)
         if value == "":
             return []
         else:
-            return value.strip().split("\n")
+            value_list = value.strip().split("\n")
+            self.logger.debug(value_list)
+            for key in invalid_list:
+                if key in value_list:
+                    value_list.remove(key)
+                if "李" in value_list and "娟" in value_list:
+                    value_list.remove("李")
+                    value_list.remove("娟")
+                    value_list.append("李娟")
+            return value_list
 
 
 
-    def _merge_sheet(self):
+    def _merge_data(self,temps):
+        '''
+        将清洗后的数据合并到一张表，并加载到dataFrame中
+        :param temps: 清洗后的数据存放的excel表名（list）
+        :return: datas合并后的数据（DataFrame格式）
+        '''
 
         datas = pd.DataFrame()
-        for sheet in self.sheets_list:
-            print(sheet)
-            # skiprows=0代表读取跳过的行数为0行，不写代表不跳过标题
-            data = pd.read_excel(self.excel_path, sheet_name=sheet, skiprows=1, index=False, encoding='utf8')
-            datas = datas.append(data)
-            print("---datas----\n",datas)
-        datas.to_excel("./333.xls",sheet_name="datas")
+        for temp in temps:
+            # 从清洗后的temp文件里获取sheet_name
+            wb_data = xlrd.open_workbook(temp)
+            sheets_list = wb_data.sheet_names()
+            for sheet in sheets_list:
+                print(sheet)
+                # skiprows=0代表读取跳过的行数为0行，不写代表不跳过标题
+                data = pd.read_excel(temp, sheet_name=sheet, skiprows=0, index=False,header=0,encoding='utf8')
+                datas = datas.append(data)
+                self.logger.debug("---datas----\n",datas)
+            # datas.to_excel(self.total,sheet_name=temp[-11:-4])
+            self.logger.warning("{}数据合并成功！！！".format(temp[-11:]))
+        # 去掉序号为0的数据（系统测试部周报则忽略第2-5行举例数据）
+        datas = datas[datas.序号 != 0]
+        # 工时里的空值用0来填充
+        datas["工时"] = datas["工时"].fillna(0)
+        # 测试人员为nan时，工时置为0
+        datas.loc[pd.isna(datas["测试人员"]) == True, "工时"] = 0
+        self.logger.warning(datas)
+        datas.to_excel(self.total, sheet_name="total")
+        return datas
 
+    # 对DataFrame中的数据进行查询、统计
+    def _data_analysis(self,datas,start,end):
+        data["期间"] = data["期间"].apply(str) #将期间列由int转为str
+        data["期间"] = pd.to_datetime(data["期间"]) # 将str转为日期格式
+        data["工时"] = data["工时"].apply(float) # 工时转为float
+        data.set_index('期间', drop=True, inplace=True) #重置索引列，并将原索引列删除
+        data["2019-08":"2019-10"] #查询
+        pd.pivot_table(data1,index=["事业部"],columns=["事业部","工作项目/任务","测试人员"],values=["测试人员","工时"],aggfunc=[np.sum])
+        data.drop(['序号', '任务来源', '任务性质', '测试负责人', '研发负责人', '本周工作目标',
+                   '实际完成时间', '实际完成情况', '亮点', '问题点', '修改内容简述', '备注'])
+        agg1 = {"工时":["sum"]}
+        result = data1.groupby(["事业部", "工作项目/任务","测试人员"]).agg(agg1)
+        result2 = data1.groupby(["事业部", "工作项目/任务"]).agg(agg1)
+        result["total"] = result2["工时"]
+        result.reset_index(level=[0, 1, 2], inplace=True)
+        result.set_index(["事业部", "工作项目/任务","测试人员","total"], drop=True, inplace=True)
+        pd.pivot_table(data1, index=["事业部", "测试产品线", "测试人员"], values=["工时"])
+        pd.pivot_table(data1, index=["事业部", "测试产品线", "测试人员"], values=["工时"], aggfunc=np.sum)
+        result.to_excel("./report.xls")
 
-    def _load_data(self):
-        '''
-        -->加载数据；
-        -->合并sheet
-        -->统计报表
-        -->写入到excel表中
-        :return:
-        '''
-        wb = xlrd.open_workbook(self.excel_path)
-        #data = pd.DataFrame(data=list(zip(ws.col_values(1), ws.col_values(2))), columns=["aa", "bb"])
-        self.logger.debug(wb.sheet_names())
-        sheets_list = wb.sheet_names()
-        # 将测试人员列、工时列多值拆成不同的行中
-        data = pd.read_excel(self.excel_path,dtype=str)
-        # 用上一行的数据替换掉na值，除第一行
-        # data = data.fillna(method ="ffill")
-        # 第一行为na的替换为空
-        data = data.fillna("")
-        # 去掉标题中的回车符与空格
-
-        # 去掉数据中空格
-        data = self._seperate_multiple_datas(data,["测试人员","工时（人日）"])
-        data1 = data["测试人员"].str.split("\n",expand=True)
-        self.logger.debug(data1)
-        data = data.reindex(columns=["测试产品线","工作项目/任务","研发\n负责人"])
-        self.logger.debug(print(data.head(22)))
-        # for sheet_index in range(len(sheets_list)):
-        #     datas = pd.read_excel(self.excel_path, sheet_name=sheet_index)
-        #     datas["日期"] = sheets_list[sheets_list]
-        #     self.logger.debug(a.split(sep="\n"))
-
-
-
-
-    def _to_excel(self):
-        pass
-
-
-    def _query_data(self):
-        pass
 
     def main(self):
-        for sheet_name in self.sheets_list:
-            self._fill_merge_cell_and_split(sheet_name)
-        self.new_wb.save("./temp/666.xls")
-        self.logger.warning("报表数据清洗成功！！！")
+        # 对周报中数据清洗、格式统一
+        self._clear_data()
+        # 合并两个部门的周报为一个excel
+        datas = self._merge_data([self.temp_xt,self.temp_ck])
+        # 查询数据出报表
+        #self._data_analysis(datas)
+
 
 if __name__ =="__main__":
     result = DataCount()
